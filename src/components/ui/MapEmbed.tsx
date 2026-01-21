cf855e9c600df4dc9849905770c6d585effccfc8
Louis Florival <ljunit25@icloud.com>
2026-01-21 15:10:09 -0500

    fix(MapEmbed): update map embed source to new location

--- src/components/ui/MapEmbed.tsx
+++ src/components/ui/MapEmbed.tsx
@@ -3,7 +3,7 @@ const MapEmbed = () => {
     <section className="mapbox" data-mapbox>
       <figure>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9%3A0x921f6aaa74197fdb!2sSacramento%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5565.017753669842!2d-73.4157067589643!3d45.78103197120126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc8ef6a64c3dba3%3A0xaa8cb00595912e94!2s1242%20Rue%20R%C3%A9migny%2C%20Repentigny%2C%20QC%20J5Y%203P9!5e0!3m2!1ses-419!2sca!4v1769025889041!5m2!1ses-419!2sca"
           width="400"
           height="300"
           loading="lazy"
