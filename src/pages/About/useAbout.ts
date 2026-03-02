9558797f5888297b154a4f6021d4dbcae146f981
Louis Florival <ljunit25@icloud.com>
2026-03-02 15:52:56 -0500

    fix: update avatar and service icons in useAbout hook for improved asset management

--- src/pages/About/useAbout.ts
+++ src/pages/About/useAbout.ts
@@ -1,5 +1,14 @@
 import { useState } from 'react';
 
+import avatar1 from '@/assets/images/avatar-1.png';
+import avatar2 from '@/assets/images/avatar-2.png';
+import avatar3 from '@/assets/images/avatar-3.png';
+import avatar4 from '@/assets/images/avatar-4.png';
+import iconApp from '@/assets/images/icon-app.svg';
+import iconDesign from '@/assets/images/icon-design.svg';
+import iconDev from '@/assets/images/icon-dev.svg';
+import iconSeo from '@/assets/images/icon-seo.svg';
+
 import useTranslation from '../../hooks/useTransalation';
 import type { CardType } from '../../types';
 
@@ -18,25 +27,25 @@ const useAbout = () => {
     {
       alt: t('webDesign'),
       title: t('webDesign'),
-      image: '/src/assets/images/icon-design.svg',
+      image: iconDesign,
       description: t('aboutPage.services.description1'),
     },
     {
       alt: t('webDevelopment'),
       title: t('webDevelopment'),
-      image: '/src/assets/images/icon-dev.svg',
+      image: iconDev,
       description: t('aboutPage.services.description2'),
     },
     {
       alt: t('mobileApps'),
       title: t('mobileApps'),
-      image: '/src/assets/images/icon-app.svg',
+      image: iconApp,
       description: t('aboutPage.services.description3'),
     },
     {
       alt: t('seo'),
       title: t('seo'),
-      image: '/src/assets/images/icon-seo.svg',
+      image: iconSeo,
       description: t('aboutPage.services.description4'),
     },
   ];
@@ -45,25 +54,25 @@ const useAbout = () => {
     {
       alt: 'Guise S',
       title: 'Guise S',
-      image: '/src/assets/images/avatar-1.png',
+      image: avatar1,
       description: t('aboutPage.testimonials.feedback1'),
     },
     {
       alt: 'Wilson Jean',
       title: 'Wilson Jean',
-      image: '/src/assets/images/avatar-4.png',
+      image: avatar4,
       description: t('aboutPage.testimonials.feedback2'),
     },
     {
       alt: 'Team Bouskad',
       title: 'Team Bouskad',
-      image: '/src/assets/images/avatar-3.png',
+      image: avatar3,
       description: t('aboutPage.testimonials.feedback3'),
     },
     {
       alt: 'JD Pet',
       title: 'JD Pet',
-      image: '/src/assets/images/avatar-2.png',
+      image: avatar2,
       description: t('aboutPage.testimonials.feedback4'),
     },
   ];
