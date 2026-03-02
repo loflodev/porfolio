7b3a3d55aa6bf0b793c3529b80f71ae2ad5bfa4b
Louis Florival <ljunit25@icloud.com>
2026-03-02 15:59:48 -0500

    fix: correct image paths in CLIENTS array for consistent asset referencing

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -11,27 +11,27 @@ import type { ClientSectionType, ContactFormInputType, PostType } from '../types
 export const CLIENTS: ClientSectionType[] = [
   {
     to: 'https://safrozen.com/',
-    image: '/src/assets/images/logo-safrozen.svg',
+    image: './src/assets/images/logo-safrozen.svg',
     alt: 'Les Aliments Safrozen Foods',
   },
   {
     to: 'https://boissonsolutionx.ca/',
-    image: '/src/assets/images/logo-boissonsolutionx.png',
+    image: './src/assets/images/logo-boissonsolutionx.png',
     alt: 'Boisson SolutionX',
   },
   {
     to: '#',
-    image: '/src/assets/images/logo-assurelis.png',
+    image: './src/assets/images/logo-assurelis.png',
     alt: 'Assurelis',
   },
   {
     to: 'https://jdpetplace.com/',
-    image: '/src/assets/images/logo-jdpetplace.png',
+    image: './src/assets/images/logo-jdpetplace.png',
     alt: '',
   },
   {
     to: '#',
-    image: '/src/assets/images/logo-5-color.png',
+    image: './src/assets/images/logo-5-color.png',
     alt: '',
   },
 ];
